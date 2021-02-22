ed4640bd2aec4249cadfea2510dc81d67afe9336
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2021-02-22 15:17:09 -0500

    final solution

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 function fizzy(num) {
-
+  if (num % 5 === 0 && num % 3 === 0) {
+    return 'FizzBuzz'
+  } else if (num % 5 === 0) {
+    return 'Buzz';
+  } else if (num % 3 === 0) {
+    return 'Fizz';
+  } else {
+    return num;
+  }
 }
+
+
 
 module.exports = {
   fizzy,
